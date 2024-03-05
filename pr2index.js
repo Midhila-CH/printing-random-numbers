838b4a97fd237b48aad8b3ba07e6099e56f167fd
Midhila-CH <101783926+Midhila-CH@users.noreply.github.com>
2024-03-05 13:21:47 +0530

    Add files via upload

---
+++ pr2index.js
@@ -0,0 +1,20 @@
+const inputvalues=document.getElementById("inputvalues");
+const inputclass=document.getElementById("inputclass");
+const number1=document.getElementById("number1");
+const number2=document.getElementById("number2");
+const number3=document.getElementById("number3");
+const addbtn=document.getElemetnById("addbtn");
+const number4=document.getElementById("number4");
+const number5=document.getElementById("number5");
+const number6=document.getElementById("number6");
+const subbtn=document.getElementById("subbtn");
+const number7=document.getElementById("number7");
+const number8=document.getElementById("number8");
+const number9=document.getElementById("number9");
+const multbtn=document.getElementById("multbtn");
+const number0=document.getElementById("number0");
+const point=document.getElementById("point");
+const dividebtn=document.getElementById("dividebtn");
+const clear=document.getElementById("clear");
+number1.onclick=document.getElementById("inputclass").innerHTML=inputclass;
+
